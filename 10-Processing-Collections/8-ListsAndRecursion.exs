6174c33656322360ce9ce5f0aecf6e258ad9eeea
pythonquick <pythonquick@yahoo.com>
2018-01-02 21:40:00 -0500

    Chapter 10 exercise updated

--- 10-Processing-Collections/8-ListsAndRecursion.exs
+++ 10-Processing-Collections/8-ListsAndRecursion.exs
@@ -3,11 +3,9 @@ defmodule Listo do
     Enum.map(orders, fn(order) ->
       state = order[:ship_to]
       net_amt = order[:net_amount]
-      total_amt = case stateTaxRates[state] do
-        nil -> net_amt
-        tax_rate -> (1 + tax_rate) * net_amt
-      end
-      order ++ [total_amount: total_amt]
+      tax = Keyword.get(stateTaxRates, state, 0)
+      total_amt = net_amt * (1+tax)
+      Keyword.put(order, :total_amount, total_amt);
     end)
   end
 
@@ -32,8 +30,8 @@ defmodule Main do
       [id: 129, ship_to: :CA, net_amount: 102.00 ],
       [id: 130, ship_to: :NC, net_amount: 50.00 ]]
     tax_rates = [NC: 0.075, TX: 0.08]
-    IO.inspect Listo.applyTaxToOrders_v1 orders, tax_rates
-    #IO.inspect Listo.applyTaxToOrders_v2 orders, tax_rates
+    #IO.inspect Listo.applyTaxToOrders_v1 orders, tax_rates
+    IO.inspect Listo.applyTaxToOrders_v2 orders, tax_rates
   end
 end
 
